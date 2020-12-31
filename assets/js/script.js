3db84e1d5cfb45e71da7667bbd3b1683e1a2f809
Snow Day <derek.miller826@gmail.com>
2020-12-31 11:45:46 -0600

    Added a form and functionality to produce a list item using a DDL and Input box.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,23 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-//check if input values are empty strings
-if (!taskNameInput || !taskTypeInput) {
-    alert("You need to fill out the task form!");
-    return false;
-}
-
 var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //check if input values are empty strings
+    if (taskNameInput === "" || !taskTypeInput === "") {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+
+    // reset form fields for next task to be entered
+    document.querySelector("input[name='task-name']").value = "";
+    document.querySelector("select[name='task-type']").selectedIndex = 0;
+
 
     //package up data as an object
     var taskDataObj = {
@@ -20,7 +27,7 @@ var taskFormHandler = function(event) {
 
     //send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
-}
+};
 
 var createTaskEl = function(taskDataObj) {
     //create list item
@@ -33,12 +40,14 @@ var createTaskEl = function(taskDataObj) {
     taskInfoEl.className = "task-info";
 
     // add HTML content to div
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
+
+    console.dir(listItemEl);
 
     // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 
 formEl.addEventListener("submit", taskFormHandler);
